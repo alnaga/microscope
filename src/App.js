32d9db66b53b64a6ce199b20fe640fc646ef2f3c
alnaga <alexgrahamnaggar@hotmail.com>
2018-08-19 22:43:32 +0100

    Added opening file dialog and begun working on rendering images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
+import fs from 'fs';
+const electron = window.require('electron');
+const { remote } = electron;
+const { dialog } = remote;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filePath: undefined,
+      showImage: false
+    }
+  }
+
+  viewImage(filePath) {
+    console.log(filePath);
+    // filePath = 'file:/' + filePath;
+    // console.log(filePath);
+
+    if(!this.state.showImage){
+      return null
+    }
+
+    return (
+      <div className="loaded-image">
+        <img alt="Content" src={filePath} />
+      </div>
+    )
+  }
+
+  openFile() {
+    let microscope = this;
+    dialog.showOpenDialog(function (file) {
+      if(file === undefined) {
+        return;
+      }
+      else{
+        microscope.setState({
+          filePath: file[0],
+          showImage: true
+        });
+      }
+    });
+  }
 
 
   render() {
+    let microscope = this;
+    let {filePath} = this.state;
 
     return (
       <div className="microscope">
-        <div class="splash">
-          <img src="./img/splash.svg" alt="Microscope Logo" class="splash-image"></img>
-          <span class="splash-title">Open an Image</span>
-        </div>
+
+        {!this.state.showImage &&
+          <div className="splash">
+            <img onClick={function(){microscope.openFile()}} src="./img/splash.svg" alt="Microscope Logo" className="splash-image" />
+            <span className="splash-title">Open an Image</span>
+          </div>
+        }
+
+        {microscope.viewImage(filePath)}
+
     </div>
     );
   }
